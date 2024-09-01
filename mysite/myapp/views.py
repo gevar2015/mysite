90b08bde5fff5ab5ee074809069fe005b395ed3c
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-09-01 17:33:30 +0300

    mysite

---
+++ mysite/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def services(request):
+    return render(request, 'myapp/services.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
