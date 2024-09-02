10fbddb68f7b0b56dd44ad0ce4ae7edb4352d0ba
gevar2015 <167052990+gevar2015@users.noreply.github.com>
2024-09-02 17:27:55 +0300

    obnovl news

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
-# Create your views here.
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'news/news_list.html', {'news': news})
+
